5e83436f25644cc4b7ab0cb9c06975f4b59dda4d
Maddy <mandarphadke1434@gmail.com>
2020-06-13 08:22:55 -0400

    second commit

--- alufab/models.py
+++ alufab/models.py
@@ -9,39 +9,38 @@ from datetime import datetime
 from django.urls import reverse
 
 
-# from datetime import DateTimefield, date, timezone
+# from datetime import DateField, date, timezone
 
 # Create your models here.
 class Employee(models.Model):
-    username =  models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=25)
-    email = models.EmailField(max_length=50)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100,default='Ichalkaranji')
-    phono = models.CharField(max_length=10)
+    Emp_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    phono = models.IntegerField(default=0)
+    Address = models.CharField(default=' ',max_length=100)
+    city = models.CharField(default='Ichalkaranji',max_length=20)
     de = [
         ('Manager','Manager'),
         ('Supervisor','Supervisor'),
         ('Admin', 'Admin')
     ]
     designation = models.CharField(max_length=10, choices=de)
+    Paymentperday = models.FloatField(default=0)
+    is_working=models.BooleanField(default=True)
+    def __int__(self):
 
-    def __str__(self):
-
-        return self.first_name
+        return self.Emp_id
 
 class customer(models.Model):
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     email = models.EmailField(max_length=50)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100,default='Ichalkaranji')
     phono = models.CharField(max_length=10)
     is_complete = models.BooleanField(default=False)
-    cust_date = models.DateTimeField(default=datetime.now, blank=True)
-    completion_date = models.DateTimeField(null=True)
+    cust_date = models.DateField(default=datetime.now, blank=True)
+    completion_date = models.DateField(null=True)
     total_payment = models.FloatField(default=0)
+    is_active=models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -51,8 +50,9 @@ class customer(models.Model):
 
 
 class measurement(models.Model):
-    mesure_date_time = models.DateTimeField(default=datetime.now, blank=True)
-    customer_name = models.ForeignKey(customer, related_name='orders', on_delete=models.CASCADE)
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    mesure_date_time = models.DateField(default=datetime.now, blank=True)
+    customer_name = models.ForeignKey(customer, on_delete=models.CASCADE)
     left = models.FloatField(default=0)
     right = models.FloatField(default=0)
     top = models.FloatField(default=0)
@@ -63,7 +63,8 @@ class measurement(models.Model):
     g2 = models.FloatField(default='0.0')
     color = models.CharField(max_length=20)
     is_complete = models.BooleanField(default=False)
-    completion_date = models.DateTimeField(null=True)
+    is_approved = models.BooleanField(default=False)
+    completion_date = models.DateField(null=True)
     Payment_per_sqft = models.FloatField(default=0)
     total_payment = models.FloatField(default=0)
 
@@ -78,6 +79,7 @@ class measurement(models.Model):
     track = models.CharField(max_length=2, choices=TRACK)
     type = models.CharField(max_length=6, choices=TY)
     area = models.FloatField(default=0)
+    cost = models.FloatField(default=0)
     def __int__(self):
         return self.customer_name
 
@@ -98,23 +100,59 @@ class worker(models.Model):
 
 
 class attendance(models.Model):
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     worker_name = models.ForeignKey(worker, on_delete=models.CASCADE)
     customer_name = models.ForeignKey(customer, on_delete=models.CASCADE)
     work_in_square_foot = models.FloatField(default=0)
     payment_per_square_ft = models.FloatField(default=0)
     total_payment = models.FloatField(default=0)
-    Date_of_attendance = models.DateTimeField(default=datetime.now, blank=True)
+    Date_of_attendance = models.DateField(default=datetime.now, blank=True)
 
     def __int__(self):
         return self.emp_id
+
+
+class material_inventory(models.Model):
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    material_name=models.CharField(max_length=20)
+    matrial_description=models.CharField(max_length=100)
+    quantity = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+    total_amt = models.FloatField(default=0)
+    is_user = models.BooleanField(default=False)
+
+class Used_material(models.Model):
+    used = models.FloatField(default=0)
+    cust_name=models.ForeignKey(customer, on_delete=models.CASCADE)
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+class Empattendance(models.Model):
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.now, blank=True)
+    Attend = models.BooleanField(default=False)
+
+class emp_pay(models.Model):
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.now, blank=True)
+    attent = models.IntegerField(default=0)
+    Payment = models.FloatField(default=0)
+
+class bill(models.Model):
+    Emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    customer_name = models.ForeignKey(customer, on_delete=models.CASCADE)
+    completion_date = models.DateField(null=True)
+    total_payment = models.FloatField(default=0)
+    paid_payment = models.FloatField(default=0)
+    payment_date = models.DateField(null=True)
+
 
 
 
 
 class customer_login(models.Model):
     cust_name = models.OneToOneField(customer,on_delete = models.CASCADE, primary_key = True)
+    cust_email = models.CharField(max_length=100)
     cust_password = models.CharField(max_length = 15)
     is_complete = models.BooleanField(default = False)
     cust_review = models.CharField(max_length = 100)
     star = models.IntegerField(default = 0)
-
+    otp = models.IntegerField(default=0)
--- alufab/urls.py
+++ alufab/urls.py
@@ -2,66 +2,149 @@ from . import views
 from django.contrib import admin
 from django.urls import path, include, URLPattern,re_path
 from django.conf.urls import url
-from .views import detailmesure, CustDelete,del_mesurements,update_measurement,del_worker,del_attendance
-
+from .views import DetailEmp,CustomerUpdate,EmployeeUpdate,WorkerUpdate
+from django.contrib.auth import views as auth_views
+#from .views import detailmesure, CustDelete,del_mesurements,update_measurement,del_worker,del_attendance
 urlpatterns = [
-    path("logout_page", views.logout_page, name='logout_page'),
-    path("", views.index, name='index'),
-    path("register", views.register, name='register'),
-    path("login", views.login, name='login'),
-    path('addcust', views.addcust,name='addcust'),
-    path('calculate_form', views.calculate_form,name='calculate_form'),
-    path('calculate_', views.calculate_,name='calculate_'),
-    path('home', views.home,name='home'),
-    path('view_customer', views.view_customer,name='view_customer'),
-    path('view_payments_workers', views.view_payments_workers,name='view_payments_workers'),
-    path('updateapyment_save/<int:id>', views.updateapyment_save, name='updateapyment_save'),
 
-
-    path('View_worker', views.View_worker,name='View_worker'),
-    path('userdet', views.userdet,name='userdet'),
-    path('view_customer', views.view_customer,name='view_customer '),
-    path('worker_save', views.worker_save,name='worker_save'),
-    path("detailmesure/<int:pk>",detailmesure.as_view(),name='detailmesure'),
-    path("update_worker/<int:pk>",views.update_worker,name='update_worker'),
-    path('emppayment', views.emppayment,name='emppayment'),
-    path('emppayform', views.emppayform,name='emppayform'),
+path("", views.index, name='index'),
+path("login_", views.login_, name='login_'),
+path("logout_page", views.logout_page, name='logout_page'),
+path("home", views.home, name='home'),
+path("register", views.register, name='register'),
+path("addcust", views.addcust, name='addcust'),
+path("calculate_form", views.calculate_form, name='calculate_form'),
+path("calculate_", views.calculate_, name='calculate_'),
+path("worker_save", views.worker_save, name='worker_save'),
+path("Inventory", views.Inventory, name='Inventory'),
+path("Worker_attend", views.Worker_attend, name='Worker_attend'),
+path("Emp_attend", views.Emp_attend, name='Emp_attend'),
+path("Emp_attend_save/<int:id>/", views.Emp_attend_save, name='Emp_attend_save'),
+path("Emp_attend_save1/<int:id>/", views.Emp_attend_save1, name='Emp_attend_save1'),
+path("otpcust", views.otpcust, name='otpcust'),
+path("custpassword/<email>/", views.custpassword, name='custpassword'),
+path("changepass/<email>/", views.changepass, name='changepass'),
 
 
 
-    path('update_emp_save/<int:pk>/<un>/', views.update_emp_save,name='update_emp_save'),
-    path('update_customer/<int:pk>/', views.update_customer,name='update_customer'),
-    re_path(r'^update_customer/(<int:pk>[0-9]+)''$',views.view_customer,),
-    path('update_customer_save/<int:pk>/', views.update_customer_save,name='update_customer_save'),
-    path('updateemp/<int:pk>/', views.update_emp,name='updateemp'),
-
-    path('userprofile', views.userprofile,name='userprofile'),
-    path('payment2', views.payment2,name='payment2'),
-
-    path('measure_list', views.measure_list,name='measure_list'),
-    path('contactview', views.contactview,name='contactview'),
-    path('customer_login_', views.customer_login_,name='customer_login_'),
-    #path('user_profile_update/<int:pk>/<un>/', views.user_profile_update, name='user_profile_update'),
-    path('complete/<int:pk>/<name>/', views.complete, name='complete'),
-    path('delete_user/<int:pk>/', views.delete_user, name='delete_user'),
-    path('filterCustomer', views.filterCustomer, name='filterCustomer'),
-
-    path("CustDelete/<int:pk>",CustDelete.as_view(),name='CustDelete'),
-    path("complete_cust/<int:pk>/",views.complete_cust,name='complete_cust'),
-    path("del_mesurements/<int:pk>",del_mesurements.as_view(),name='del_mesurements'),
-    path("update_measurement/<int:pk>",update_measurement.as_view(),name='update_measurement'),
-    path("del_mesurements/<int:pk>",del_mesurements.as_view(),name='del_mesurements'),
-    path("del_worker/<int:pk>",del_worker.as_view(),name='del_worker'),
-    path("del_attendance/<int:pk>",del_attendance.as_view(),name='del_attendance'),
-    path("customer_review/<int:id>/",views.customer_review,name='customer_review'),
-    path("customer_home_/<int:id>/", views.customer_home_, name='customer_home_'),
-    path("GeneratePdf/<int:id>/", views.GeneratePdf, name='GeneratePdf'),
+path("forgotpasscust", views.forgotpasscust, name='forgotpasscust'),
 
 
 
 
 
 
+
+path("filterCustomer", views.filterCustomer, name='filterCustomer'),
+path("filterEmppay", views.filterEmppay, name='filterEmppay'),
+path("filterWorkerpay", views.filterWorkerpay, name='filterWorkerpay'),
+path("filtermes", views.filtermes, name='filtermes'),
+
+path("customer_review/<int:id>/",views.customer_review,name='customer_review'),
+path("customer_home_/<int:id>/", views.customer_home_, name='customer_home_'),
+path("GeneratePdf/<int:id>/", views.GeneratePdf, name='GeneratePdf'),
+path("genratebill/<int:cust>/", views.genratebill, name='genratebill'),
+
+path("customer_login_", views.customer_login_, name='customer_login_'),
+path("emppayment", views.emppayment, name='emppayment'),
+path("emppayform", views.emppayform, name='emppayform'),
+
+path('complete/<int:pk>/<name>/', views.complete, name='complete'),
+path("DetailEmp/<pk>/", DetailEmp.as_view(), name='DetailEmp'),
+
+
+path("DetailEmps", views.DetailEmps, name='DetailEmps'),
+path("DetailCust", views.DetailCust, name='DetailCust'),
+path('DetailInv', views.DetailInv,name='DetailInv'),
+path('DetailInv0', views.DetailInv0,name='DetailInv0'),
+path('DetailInv1', views.DetailInv1,name='DetailInv1'),
+
+path("CustomerUpdate/<pk>/", CustomerUpdate.as_view(), name='CustomerUpdate'),
+path("EmployeeUpdate/<pk>/", EmployeeUpdate.as_view(), name='EmployeeUpdate'),
+path("WorkerUpdate/<pk>/", WorkerUpdate.as_view(), name='WorkerUpdate'),
+path("UpdateInventory/<int:id>/", views.UpdateInventory, name='UpdateInventory'),
+
+
+
+path('DetailWorkAttend', views.DetailWorkAttend,name='DetailWorkAttend'),
+path('DetailWork', views.DetailWork,name='DetailWork'),
+path('DetailAttend', views.DetailAttend,name='DetailAttend'),
+path('AttendDate/<date>/', views.AttendDate,name='AttendDate'),
+path('AttendEmp', views.AttendEmp,name='AttendEmp'),
+path('AttendEmpdetail/<id>/', views.AttendEmpdetail,name='AttendEmpdetail'),
+path('Emppay', views.Emppay,name='Emppay'),
+path('Emppaycalc/<id>/', views.Emppaycalc,name='Emppaycalc'),
+path('Approved/<id>/', views.Approved,name='Approved'),
+path('mesdetail/<id>/', views.mesdetail,name='mesdetail'),
+path('addcost/<id>/',views.addcost,name='addcost'),
+path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
+path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+# url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+# url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+# url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
+# url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 
 ]
+# urlpatterns = [
+#     path("logout_page", views.logout_page, name='logout_page'),
+#     path("", views.index, name='index'),
+#     path("register", views.register, name='register'),
+#     path("login", views.login, name='login'),
+#     path('addcust', views.addcust,name='addcust'),
+#     path('calculate_form', views.calculate_form,name='calculate_form'),
+#     path('calculate_', views.calculate_,name='calculate_'),
+#     path('home', views.home,name='home'),
+#     path('view_customer', views.view_customer,name='view_customer'),
+#     path('view_payments_workers', views.view_payments_workers,name='view_payments_workers'),
+#     path('updateapyment_save/<int:id>', views.updateapyment_save, name='updateapyment_save'),
+#
+#
+#     path('View_worker', views.View_worker,name='View_worker'),
+#     path('detailUser', views.detailUser,name='detailUser'),
+#     path('view_customer', views.view_customer,name='view_customer '),
+#     path('worker_save', views.worker_save,name='worker_save'),
+#     path("detailmesure/<int:pk>",detailmesure.as_view(),name='detailmesure'),
+#     path("update_worker/<int:pk>",views.update_worker,name='update_worker'),
+#     path('emppayment', views.emppayment,name='emppayment'),
+#     path('emppayform', views.emppayform,name='emppayform'),
+#
+#
+#
+#     path('update_emp_save/<int:pk>/<un>/', views.update_emp_save,name='update_emp_save'),
+#     path('update_customer/<int:pk>/', views.update_customer,name='update_customer'),
+#     re_path(r'^update_customer/(<int:pk>[0-9]+)''$',views.view_customer,),
+#     path('update_customer_save/<int:pk>/', views.update_customer_save,name='update_customer_save'),
+#
+#     path('userprofile', views.userprofile,name='userprofile'),
+#     path('payment2', views.payment2,name='payment2'),
+#
+#     path('measure_list', views.measure_list,name='measure_list'),
+#     path('contactview', views.contactview,name='contactview'),
+#     path('customer_login_', views.customer_login_,name='customer_login_'),
+#     #path('user_profile_update/<int:pk>/<un>/', views.user_profile_update, name='user_profile_update'),
+#     path('complete/<int:pk>/<name>/', views.complete, name='complete'),
+#     path('Detail_user/<int:pk>/', views.Detail_user, name='Detail_user'),
+#     path('filterCustomer', views.filterCustomer, name='filterCustomer'),
+#
+#     path("CustDelete/<int:pk>",CustDelete.as_view(),name='CustDelete'),
+#     path("complete_cust/<int:pk>/",views.complete_cust,name='complete_cust'),
+#     path("del_mesurements/<int:pk>",del_mesurements.as_view(),name='del_mesurements'),
+#     path("update_measurement/<int:pk>",update_measurement.as_view(),name='update_measurement'),
+#     path("del_mesurements/<int:pk>",del_mesurements.as_view(),name='del_mesurements'),
+#     path("del_worker/<int:pk>",del_worker.as_view(),name='del_worker'),
+#     path("del_attendance/<int:pk>",del_attendance.as_view(),name='del_attendance'),
+#     path("customer_review/<int:id>/",views.customer_review,name='customer_review'),
+#     path("customer_home_/<int:id>/", views.customer_home_, name='customer_home_'),
+#     path("GeneratePdf/<int:id>/", views.GeneratePdf, name='GeneratePdf'),
+#
+#
+#
+#
+#
+#
+#
+#
+# ]
